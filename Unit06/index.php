eb8cc7c95fc05acd0989520f9c247b50dbefaccf
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2017-12-07 15:41:02 +0000

    Removed message file from 5 and 6 that wasn't needed. Changed table used in Assignment 6.

---
+++ Unit06/index.php
@@ -0,0 +1,61 @@
+<?php include'header.php' ?>
+
+    <main role="main" class="container">
+
+<?php include'navigation.php' ?>
+
+      <div class="starter-template">
+        <h1>Submit Recipe</h1>
+
+<form id="recipe" action="addrecipe.php" method="post" enctype="multipart/form-data">
+
+  <div class="form-group">
+    <label for="nameinput">Recipe Name</label>
+    <input type="text" class="form-control" id="nameinput" name="recipename" placeholder="Smoked Chicken">
+  </div>
+
+  <div class="form-group">
+    <label for="cooktime">Cook Time</label>
+    <input type="text" class="form-control" id="cooktime" name="cooktime" placeholder="30 Minutes">
+  </div>
+
+  <div class="form-group">
+    <label for="ingredients">Ingredients</label>
+    <textarea type="text" class="form-control" rows="6" id="ingredients" name="ingredients" placeholder="4 cups water"></textarea>
+  </div>
+
+  <div class="form-group">
+    <label for="directions">Directions</label>
+    <textarea type="text" class="form-control" rows="6" id="directions" name="directions" placeholder="Mix water, milk, and sugar in pot."></textarea>
+  </div>
+
+  <div class="form-group">
+    <label for="picture">Picture</label>
+    <input type="file" class="form-control-file" id="picture" name="picture">
+    <br>
+    <p>Image must be .jpg, .png, or .gif. Size must be less than 1mb and less than 200px X 200px.</p>
+  </div>
+
+  <div class="form-group">
+    <label for="author">Author</label>
+    <input type="text" class="form-control" id="author" name="author" placeholder="John Smith">
+  </div>
+
+
+
+<button type="submit" form="recipe" value="submit" class="primary-btn btn btn-lg">Submit</button>
+</form>	
+
+
+      </div>
+
+    </main><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+  </body>
+</html>
---
+++ Unit06/listrecipes.php
@@ -0,0 +1,74 @@
+<?php include'../../includes/connect.php'?>
+<?php include'header.php'?>
+
+    <main role="main" class="container">
+
+<?php include'navigation.php'?>
+
+      <div class="starter-template">
+        <h1>Manage Recipes</h1>
+
+<form id="managerecipes" action="managerecipes.php" method="post">
+  <div class="form-group">
+<br>
+
+<?php
+
+$query = "select * from recipessix";
+
+$result = mysqli_query($conn, $query);
+
+while ($row = mysqli_fetch_array($result))
+{
+   $resultArr[] = $row;
+}
+
+?>
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Image</th>
+      <th scope="col">Recipe Name</th>
+      <th scope="col">Cooktime</th>
+      <th scope="col">Author</th>
+      <th scope="col">Manage</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php foreach($resultArr as $output): ?>
+    <tr>
+    <td><img src="<?php echo $output['image']?>" alt="" class="img-thumbnail small-thumb"></td>
+    <td><?php echo $output['name']?></td>
+    <td><?php echo $output['cooktime']?></td>
+    <td><?php echo $output['author']?></td>
+    <td>
+      <button name="details" type="submit" form="managerecipes" value="<?php echo $output['id'];?>" class="primary-btn btn btn-sm">Details</button>
+      <button name="delete" type="submit" form="managerecipes" value="<?php echo $output['id'];?>" class="primary-btn btn btn-sm">Delete</button>
+      <button name="update" type="submit" form="managerecipes" value="<?php echo $output['id'];?>" class="primary-btn btn btn-sm">Update</button>
+    </td>
+    </tr>
+<?php endforeach; ?>
+
+  </tbody>
+</table>
+
+
+  </div>
+
+</form>	
+
+
+      </div>
+
+    </main><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+  </body>
+</html>
