ac00096211995858b8b1961c0afa81a4e3cd402c
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2017-12-08 14:22:05 +0000

    Added Unit 8 start files.

---
+++ Unit08/navigation.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <?php if(isset($_SESSION['username'])): ?>
+      <a class="nav-item nav-link" href="addemployee.php">Add Employee</a>
+      <a class="nav-item nav-link" href="admin.php">Manage Employees</a>
+      <a class="nav-item nav-link" href="logout.php">Logout</a>
+      <p class="nav-item nav-link">Logged in as <?php echo $_SESSION['username']; ?></p>
+      <?php else: ?>
+      <a class="nav-item nav-link" href="index.php">Login</a>
+      <a class="nav-item nav-link" href="register.php">Register</a>
+      <?php endif; ?>
+    </div>
+  </div>
+</nav>
---
+++ Unit08/success.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo "<h2>Your account has been created and you may now log in!</h2>";
+
+
+?>
+
+
+<meta http-equiv="refresh" content="3; url=index.php" />
